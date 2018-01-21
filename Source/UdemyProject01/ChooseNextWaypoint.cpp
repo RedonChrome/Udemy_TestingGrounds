3a118921884662186722d7fde1566061ba15ed76
Emil <Emilarostad@gmail.com>
2018-01-21 23:50:23 +0100

    Created PatrolPointsCPP, migrating AI to CPP

--- Source/UdemyProject01/ChooseNextWaypoint.cpp
+++ Source/UdemyProject01/ChooseNextWaypoint.cpp
@@ -2,11 +2,14 @@
 
 #include "ChooseNextWaypoint.h"
 #include "UdemyProject01.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-	UE_LOG(LogTemp, Warning, TEXT("AI in C++ - yay!"));
+	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	UE_LOG(LogTemp, Warning, TEXT("Waypoint Index: %i"), Index);
 	return EBTNodeResult::Succeeded;
 }
